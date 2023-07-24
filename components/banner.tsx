db10b1b01898159f4fc5ebd6c3d849bdfa94db28
Arpan M <121425759+grilledpython@users.noreply.github.com>
2023-07-24 19:17:20 +0530

    Update banner.tsx

--- components/banner.tsx
+++ components/banner.tsx
@@ -13,15 +13,15 @@ export default function Banner() {
             <div className="text-slate-500 inline-flex">
               <a
                 className="font-medium hover:underline text-slate-50"
-                href="https://github.com/cruip/open-react-template"
+                href="https://arpan.codes/discord"
                 target="_blank"
                 rel="noreferrer"
               >
-                Download<span className="hidden sm:inline"> on GitHub</span>
+                Join<span className="hidden sm:inline"> Discord</span>
               </a>{' '}
               <span className="italic px-1.5">or</span>{' '}
-              <a className="font-medium hover:underline text-emerald-400" href="https://cruip.com/open-pro/" target="_blank" rel="noreferrer">
-                Check Premium Version
+              <a className="font-medium hover:underline text-emerald-400" href="https://arpan.codes/discord" target="_blank" rel="noreferrer">
+                Contact Us
               </a>
             </div>
             <button className="text-slate-500 hover:text-slate-400 pl-2 ml-3 border-l border-gray-700" onClick={() => setBannerOpen(false)}>
